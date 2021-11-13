5cd2366599e7a3ee82ed547cdd201a32e9c5c13a
surajdhaygude <surajdhaygude2603@gmail.com>
2021-11-13 11:38:17 +0530

    Second Commit

--- CTGeneralHospital/src/app/app.module.ts
+++ CTGeneralHospital/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './userModule/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
