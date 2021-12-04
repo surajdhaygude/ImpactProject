dd49544e082115f99a3a572e4a99b0614563bfb4
surajdhaygude <surajdhaygude2603@gmail.com>
2021-12-04 18:23:02 +0530

    PatientUser Added

---
+++ PracticeManagementSystem/src/app/models/Patientusers.ts
@@ -0,0 +1,11 @@
+export class PatientUser {
+    id:number=0;
+    PatientId : number=0;
+    PatientName: string="";
+    DateofJoining:Date=new Date();
+    status: string="";
+    EditStatus:string="";
+    Manage:string="";
+    emailid:string="";
+    number:number=0;
+  }
--- PracticeManagementSystem/src/app/modules/adminmodule/patientusermanagement/patientusermanagement.component.ts
+++ PracticeManagementSystem/src/app/modules/adminmodule/patientusermanagement/patientusermanagement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PatientService } from 'src/app/Services/patient.service';
 
 @Component({
   selector: 'app-patientusermanagement',
@@ -7,9 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientusermanagementComponent implements OnInit {
 
-  constructor() { }
-
+  
+  constructor(private patientservice:PatientService, private route:Router) { }
+  PatientName:any;
+  p:number=1;
+  Managelist:any[]=["Active","Deactivate","Block"]
+  btnValue : string = "";
+  btnValue1 : string = "";
+ 
+  
   ngOnInit(): void {
+    this.GetAllPatientsList();
   }
+
+  PatientData:any[]=[];
+  GetAllPatientsList() {
+    this.patientservice.getPatientList().subscribe((data: any[]) => {
+      console.log(data);
+      this.PatientData=data;
+    })
+    }
+
+
+    search(){
+      if(this.PatientName== ""){
+        this.ngOnInit()
+      }else{
+        this.PatientData=this.PatientData.filter(res =>{
+          return res.PatientName.toLocaleLowerCase().match(this.PatientName.toLocaleLowerCase());
+        })
+      }
+  
+    }
+    key: string ='PatientId';
+    reverse:boolean=false;
+    sort(key: string){
+      this.key=key;
+      this.reverse=!this.reverse;
+    }
+
+    Addpatientrecord():void{
+      this.route.navigateByUrl('addpatient');
+     }
+
+     DispayPatientGrid():void{
+      this.route.navigateByUrl('hospitalusermanagement');
+     }
+
+     deleteuser(id:number){
+      if(confirm('Are you sure to delete physician record?')){
+        this.patientservice.Husersdelete(id).subscribe(res =>{});
+        this.patientservice.getPatientList().subscribe(data =>{
+          this.PatientData=data;
+        });
+      }
+    }
+    edituser(id:number){
+      this.route.navigate(['/editPatient', id]);
+    }
+    getCurrentStatus(){
+      
+    }
+ 
 
 }
