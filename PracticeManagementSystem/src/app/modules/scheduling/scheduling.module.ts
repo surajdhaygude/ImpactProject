d8740e9ecd9b289d2a40efd51c101346e8a22a6e
surajdhaygude <surajdhaygude2603@gmail.com>
2021-12-20 10:34:42 +0530

    medication changes

--- PracticeManagementSystem/src/app/modules/scheduling/scheduling.module.ts
+++ PracticeManagementSystem/src/app/modules/scheduling/scheduling.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientschedulingComponent } from './patientscheduling/patientscheduling.component';
 import { UserschedulingComponent } from './userscheduling/userscheduling.component';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +14,13 @@ import { UserschedulingComponent } from './userscheduling/userscheduling.compone
     UserschedulingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      {path:'user', component:UserschedulingComponent}
+    ])
   ]
 })
 export class SchedulingModule { }
