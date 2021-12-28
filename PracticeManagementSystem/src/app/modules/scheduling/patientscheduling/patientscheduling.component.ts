5ce2f8f1c9794063fdd8767fdd86f553e6306630
AtishLangote <atishlangote28@gmail.com>
2021-12-28 11:29:58 +0530

    SchedulingAdd

--- PracticeManagementSystem/src/app/modules/scheduling/patientscheduling/patientscheduling.component.ts
+++ PracticeManagementSystem/src/app/modules/scheduling/patientscheduling/patientscheduling.component.ts
@@ -1,6 +1,8 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import{EventSettingsModel, View} from '@syncfusion/ej2-angular-schedule';
+import { SchedulingService } from 'src/app/scheduling.service';
 
 @Component({
 
@@ -12,38 +14,58 @@ import{EventSettingsModel, View} from '@syncfusion/ej2-angular-schedule';
 })
 export class PatientschedulingComponent implements OnInit {
 
-  constructor(private router:Router) { }
+    calenderData:any[]=[]
+  Id:number=0;
+  //createdBy:number=0;
+  Subject:string="";
+  StartTime:string="";
+  EndTime:string="";
+
+  constructor(private router:Router,private service:SchedulingService) { }
 
   ngOnInit(): void {
+    this.service.GetPatientCalenderData().subscribe(
+        (data) => {
+          debugger
+          //this.calenderData = data;
+          this.Id=data.createdBy;
+          this.Subject=data.meetingTitle;
+          this.StartTime=data.timeOfAppointment;
+          this.EndTime=data.timeOfAppointment;
+
+        })
   }
 //   public selectedDate: Date = new Date(2021, 12, 18);
 public eventSettings: EventSettingsModel = {
     dataSource: [
-    {
-        Id: 1,
-        Subject: 'Explosion of Betelgeuse Star',
-        StartTime: new Date(2021, 12, 15, 9, 30),
-        EndTime: new Date(2021, 12, 15, 11, 0)
-    }, {
-        Id: 2,
-        Subject: 'Thule Air Crash Report',
-        StartTime: new Date(2021, 12, 12, 12, 0),
-        EndTime: new Date(2021, 12, 12, 14, 0)
-    }, {
-        Id: 3,
-        Subject: 'Blue Moon Eclipse',
-        StartTime: new Date(2021, 12 ,13, 9, 30),
-        EndTime: new Date(2021, 12, 13, 11, 0)
-    }, {
-        Id: 4,
-        Subject: 'Meteor Showers in 2018',
-        StartTime: new Date(2021, 12, 14, 13, 0),
-        EndTime: new Date(2021, 12, 14, 14, 30)
-    }]
+    { 
+        Id:this.Id,
+        Subject:this.Subject,
+       //Subject: 'Thule Air Crash Report',
+        StartTime: new Date(2021, 11, 21, 2, 0),
+        EndTime: new Date(2021, 11, 21, 3,0)
+    }
+    // , {
+    //     Id: 2,
+    //     Subject: 'Thule Air Crash Report',
+    //     StartTime: new Date(2021, 12, 12, 12, 0),
+    //     EndTime: new Date(2021, 12, 12, 14, 0)
+    // }, {
+    //     Id: 3,
+    //     Subject: 'Blue Moon Eclipse',
+    //     StartTime: new Date(2021, 12 ,13, 9, 30),
+    //     EndTime: new Date(2021, 12, 13, 11, 0)
+    // }, {
+    //     Id: 4,
+    //     Subject: 'Meteor Showers in 2018',
+    //     StartTime: new Date(2021, 12, 14, 13, 0),
+    //     EndTime: new Date(2021, 12, 14, 14, 30)
+    // }
+]
 };
  
 
-public selectedDate: Date = new Date(2021, 12, 18);
+public selectedDate: Date = new Date(2021, 12, 27);
 public currentView: View = 'Month';
 
 backtoDashbors(){
